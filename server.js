ab9deb868dcd1dfe568ff8795316084836badfa1
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-07-15 15:17:55 -0300

    Dynamic Version

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 app.set("view engine", "ejs")
 app.use(expressLayouts)
@@ -20,9 +21,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 //Index route
-app.get("/", function(req,res){
-  res.render("index", {title: "CSE Motors"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
